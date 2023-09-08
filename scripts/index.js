41180648f6f0b37bb5e4ba27018f5716cb8e88ef
VeselovTimofey <vestimofey@mail.ru>
2023-09-09 00:37:39 +0300

    v4.0

--- scripts/index.js
+++ scripts/index.js
@@ -9,30 +9,16 @@ const buttonOpenPopupAddNewCard = profile.querySelector('.profile__add-button');
 
 const popupProfile = document.querySelector('.popup_type_profile');
 const buttonClosePopupProfile = popupProfile.querySelector('.popup__close-icon');
-<<<<<<< HEAD
-const formPopupProfile = popupProfile.querySelector('.popup__form');
 const inputNamePopupProfile = popupProfile.querySelector('.popup__input_value_name');
 const inputJobPopupProfile = popupProfile.querySelector('.popup__input_value_profession');
-const buttonSavePopupProfile = popupProfile.querySelector('.popup__button');
-=======
-const inputNamePopupProfile = popupProfile.querySelector('.popup__input_value_name');
-const inputJobPopupProfile = popupProfile.querySelector('.popup__input_value_profession');
->>>>>>> develop
 
 const popupAddNewCard = document.querySelector('.popup_type_add-card');
 const buttonClosePopupAddNewCard = popupAddNewCard.querySelector('.popup__close-icon');
 const formPopupAddNewCard = popupAddNewCard.querySelector('.popup__form');
 const inputPlacePopupAddNewCard = popupAddNewCard.querySelector('.popup__input_value_place');
 const inputImagePopupAddNewCard = popupAddNewCard.querySelector('.popup__input_value_image');
-<<<<<<< HEAD
-const buttonSavePopupAddNewCard = popupAddNewCard.querySelector('.popup__button');
 
 const cards = document.querySelector('.elements');
-const newCard = document.querySelector('#element').content;
-=======
-
-const cards = document.querySelector('.elements');
->>>>>>> develop
 
 const popupFullImage = document.querySelector('.popup_type_picture');
 const picturePopupFullImage = popupFullImage.querySelector('.popup__image');
@@ -59,27 +45,15 @@ function closePopupWithEscape(evt) {
 function openPopupProfile() {
     inputNamePopupProfile.value = nameProfile.textContent;
     inputJobPopupProfile.value = jobProfile.textContent;
-<<<<<<< HEAD
-    hideInputError(formPopupProfile, inputNamePopupProfile, config);
-    hideInputError(formPopupProfile, inputJobPopupProfile, config);
-    toggleButtonState([inputNamePopupProfile, inputJobPopupProfile], buttonSavePopupProfile, config);
-=======
     const profileFormValidator = new FormValidator(config, popupProfile);
     profileFormValidator.enableValidation();
->>>>>>> develop
     openPopup(popupProfile);
 };
 
 function openPopupAddNewCard() {
     formPopupAddNewCard.reset();
-<<<<<<< HEAD
-    hideInputError(formPopupAddNewCard, inputPlacePopupAddNewCard, config);
-    hideInputError(formPopupAddNewCard, inputImagePopupAddNewCard, config);
-    toggleButtonState([inputPlacePopupAddNewCard, inputImagePopupAddNewCard], buttonSavePopupAddNewCard, config);
-=======
     const newCardFormValidator = new FormValidator(config, popupAddNewCard);
     newCardFormValidator.enableValidation();
->>>>>>> develop
     openPopup(popupAddNewCard);
 };
 
@@ -105,40 +79,17 @@ function handleNewCardFormSubmit(evt) {
 };
 
 function InitializationNewCard(initialElement) {
-<<<<<<< HEAD
-    const newElement = newCard.querySelector('.element').cloneNode(true);
-    const imageNewElement = newElement.querySelector('.element__image');
-    imageNewElement.src = initialElement['link'];
-    imageNewElement.alt = initialElement['name'];
-    newElement.querySelector('.element__name').textContent = initialElement['name'];
-    newElement.querySelector('.element__like').addEventListener('click', (evt) => {
-        evt.target.classList.toggle('element__like_active');
-    });
-    newElement.querySelector('.element__delete').addEventListener('click', () => {
-        newElement.remove();
-    });
-    imageNewElement.addEventListener('click', () => {
-        openPopupFullImage(initialElement['link'], initialElement['name'])
-    });
-    return newElement;
-=======
     const card = new Card(initialElement, '#element');
     const cardElement = card.generateCard();
     cardElement.querySelector('.element__image').addEventListener('click', () => {
         openPopupFullImage(initialElement['link'], initialElement['name'])
     });
     return cardElement;
->>>>>>> develop
 };
 
 function initializationFirstElements() {
     for (const initialElement of initialElements) {
-<<<<<<< HEAD
-        const newElement = InitializationNewCard(initialElement);
-        cards.append(newElement);
-=======
         cards.append(InitializationNewCard(initialElement));
->>>>>>> develop
     };
 };
 
